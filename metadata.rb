036ab92d61043a9f3966fbf7d8daa0bc1a912192
105325 <scott.marshall@johnmuirhealth.com>
2017-05-23 09:12:14 -0700

    Removed license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,6 @@
 name 'pingfederate'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
-license 'all_rights'
 description 'Installs/Configures pingfederate'
 long_description 'Installs/Configures pingfederate'
 version '0.1.0'
