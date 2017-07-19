6f94cf6f2f900965f2e1f4cc9d4c03f7ea045a12
melindam <melinda.moran@johnmuirhealth.com>
2017-07-19 10:16:20 -0700

    update to cookbook version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer 'The Authors'
 maintainer_email 'you@example.com'
 description 'Installs/Configures pingfederate'
 long_description 'Installs/Configures pingfederate'
-version '0.1.0'
+version '0.1.1'
 
 depends 'java'
